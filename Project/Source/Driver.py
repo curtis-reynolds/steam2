776dda346918e6523c0f411eb9c07a4fdf6ea3f4
Abdullah Mohammed <67016379+Abdullah-Mohammed-OTU@users.noreply.github.com>
2024-03-13 14:40:00 -0400

    Update Driver.py
    minor change in the naming of the files

--- Project/Source/Driver.py
+++ Project/Source/Driver.py
@@ -2,9 +2,9 @@ import Transactions
 
 def main():
     #Main function to load, process, and append transactions.
-    user_accounts_file = 'Project/Source/user_accounts1.txt'
-    available_games_file = 'Project/Source/availablegames1.txt'
-    games_collection_file = 'Project/Source/gamescollection1.txt'
+    user_accounts_file = 'Project/Source/user_accounts.txt'
+    available_games_file = 'Project/Source/availablegames.txt'
+    games_collection_file = 'Project/Source/gamescollection.txt'
     daily_transactions_file = 'Project/Source/daily_transactions.txt'
 
     # Process transactions and append them to the respective files
